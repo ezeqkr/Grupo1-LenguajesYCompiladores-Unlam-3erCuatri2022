52e395f2e7e31a2ed0a2d8f9f4ca66e74ea187e0
ezeqkr <42724119+ezeqkr@users.noreply.github.com>
2023-02-04 11:54:43 -0300

    Initial commit

---
+++ src/test/java/lyc/compiler/Constants.java
@@ -0,0 +1,6 @@
+package lyc.compiler;
+
+public final class Constants {
+    public static final String EXAMPLES_ROOT_DIRECTORY = "https://raw.githubusercontent.com/Lenguajes-y-Compiladores-UnLaM/consignas-tp/main/ejemplos";
+
+}
---
+++ src/test/java/lyc/compiler/ParserTest.java
@@ -0,0 +1,101 @@
+package lyc.compiler;
+
+import java_cup.runtime.Symbol;
+import lyc.compiler.factories.ParserFactory;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import static com.google.common.truth.Truth.assertThat;
+import static lyc.compiler.Constants.EXAMPLES_ROOT_DIRECTORY;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Disabled
+public class ParserTest {
+
+    @Test
+    public void assignmentWithExpression() throws Exception {
+        compilationSuccessful("c=d*(e-21)/4");
+    }
+
+    @Test
+    public void syntaxError() {
+        compilationError("1234");
+    }
+
+    @Test
+    void assignments() throws Exception {
+        compilationSuccessful(readFromFile("assignments.txt"));
+    }
+
+    @Test
+    void write() throws Exception {
+        compilationSuccessful(readFromFile("write.txt"));
+    }
+
+    @Test
+    void read() throws Exception {
+        compilationSuccessful(readFromFile("read.txt"));
+    }
+
+    @Test
+    void comment() throws Exception {
+        compilationSuccessful(readFromFile("comment.txt"));
+    }
+
+    @Test
+    void init() throws Exception {
+        compilationSuccessful(readFromFile("init.txt"));
+    }
+
+    @Test
+    void and() throws Exception {
+        compilationSuccessful(readFromFile("and.txt"));
+    }
+
+    @Test
+    void or() throws Exception {
+        compilationSuccessful(readFromFile("or.txt"));
+    }
+
+    @Test
+    void not() throws Exception {
+        compilationSuccessful(readFromFile("not.txt"));
+    }
+
+    @Test
+    void ifStatement() throws Exception {
+        compilationSuccessful(readFromFile("if.txt"));
+    }
+
+    @Test
+    void whileStatement() throws Exception {
+        compilationSuccessful(readFromFile("while.txt"));
+    }
+
+
+    private void compilationSuccessful(String input) throws Exception {
+        assertThat(scan(input).sym).isEqualTo(ParserSym.EOF);
+    }
+
+    private void compilationError(String input){
+        assertThrows(Exception.class, () -> scan(input));
+    }
+
+    private Symbol scan(String input) throws Exception {
+        return ParserFactory.create(input).parse();
+    }
+
+    private String readFromFile(String fileName) throws IOException {
+        URL url = new URL(EXAMPLES_ROOT_DIRECTORY + "/%s".formatted(fileName));
+        assertThat(url).isNotNull();
+        return IOUtils.toString(url.openStream(), StandardCharsets.UTF_8);
+    }
+
+
+}
