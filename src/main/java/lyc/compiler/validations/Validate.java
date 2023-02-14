f0600e30f5563b21a8fe42aca7e1839349853564
Ezequiel <restoveganlist@gmail.com>
2023-02-14 11:50:50 -0300

    pull

--- src/main/java/lyc/compiler/validations/Validate.java
+++ src/main/java/lyc/compiler/validations/Validate.java
@@ -2,6 +2,9 @@ package lyc.compiler.validations;
 
 public class Validate {
 
+    final static int rangoString = 40;	
+    final static int rangoIdentifier = 30;	
+
     public static void validateInt(String yytext) {
         int entero = Integer.parseInt(yytext);
         if (entero < -32768 || entero > 32767) {
@@ -15,15 +18,23 @@ public class Validate {
         long cota_i = -2147483648;
         long cota_s = 2147483647;
         if (flotante <= cota_i || flotante >= cota_s) {
-            System.out.println("\nERROR LEXICO : Rango reales Invalido (32 bits):" + yytext + "%s\n");
+            System.out.println("\nERROR LEXICO : Rango reales Invalido (32 bits):" + yytext + "\n");
             System.exit(0);
         }
     }
 
     public static void validateString(String yytext) {
         int cadena = yytext.length();
-        if (cadena > 30) {
-            System.out.println("\nERROR LEXICO : Longitud String Invalido ( 30 caracteres ):" + yytext + " %s\n");
+        if (cadena > rangoString - 2 ) {
+            System.out.println("\nERROR LEXICO : Longitud String Invalido ( 40 caracteres ):" + yytext + "\n");
+            System.exit(0);
+        }
+    }
+    
+    public static void validateIdentifier(String yytext) {
+        int cadena = yytext.length();
+        if (cadena > rangoIdentifier - 2 ) {
+            System.out.println("\nERROR LEXICO : Longitud de Identificador Invalido ( 30 caracteres ):" + yytext + "\n");
             System.exit(0);
         }
     }
