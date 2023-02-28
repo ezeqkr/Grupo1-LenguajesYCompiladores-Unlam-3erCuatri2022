b80b1f1602a5731210ece25a4e542a88d3814ff6
martinngreco <martin00greco@gmail.com>
2023-02-28 15:51:26 -0300

    pre pull main 2da entrega

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -1,12 +1,42 @@
 package lyc.compiler.files;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class IntermediateCodeGenerator implements FileGenerator {
 
+    private ArrayList<String> lista = new ArrayList<String>();
+    public IntermediateCodeGenerator(ArrayList<String> listaPolacaInversa){
+        this.lista = listaPolacaInversa;
+    }
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+
+            BufferedWriter bw = null;
+            try {
+
+                bw = new BufferedWriter(fileWriter);
+                int i = 1;
+
+                for(String celda : lista){
+                    bw.write(i + ". " + celda + "\n");
+                    i++;
+                }
+                i=1;
+                for(String celda : lista){
+                    bw.write("|" + celda);
+                    i++;
+                }
+                bw.write("|");
+
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                bw.close();
+            }
+
     }
 }
