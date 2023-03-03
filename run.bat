e2ac7f99774d3e80fc1af803b81c1eeb4ebe1b0d
Ezequiel <restoveganlist@gmail.com>
2023-03-03 09:50:24 -0300

    run.bat actualizdo

--- run.bat
+++ run.bat
@@ -2,3 +2,8 @@ echo "Compiling run.txt"
 java "-jar" "target\lyc-compiler-3.0.0.jar" "target\input\test.txt"
 COPY  "target\output\final.asm"
 tasm final.asm
+tlink final.obj
+final.exe
+del final.obj
+del final.exe
+del final.map
