9b1f3eb21fd34ebcc4e511ecc0a30808af8a84e2
LorenaCh <rocio.lorena18@gmail.com>
2023-03-03 11:00:47 -0300

    arreglo txt

--- final.asm
+++ final.asm
@@ -4,16 +4,17 @@
 
 .DATA
 
-b                    dd    ?                             
 z                    dd    ?                             
 a                    dd    ?                             
 d                    dd    ?                             
 x                    dd    ?                             
 e                    dd    ?                             
+b                    dd    ?                             
 c                    dd    ?                             
+_3                   dd    3                             
+_4                   dd    4                             
 _1                   dd    1                             
 _0                   dd    0                             
-_3                   dd    3                             
 _100                 dd    100                           
 _2                   dd    2                             
 @retorno             dd    ?                             
@@ -25,6 +26,12 @@ _2                   dd    2
 MOV AX, @DATA
 MOV DS, AX
 MOV ES, AX
+
+FLD _3
+FSTP a
+
+FLD _4
+FSTP d
 
 FLD _1
 FSTP x
@@ -50,15 +57,21 @@ FSTP a
 
 etiqueta1:
 FLD _2
-FSTP a
-
-FLD _2
 FSTP d
+
+FLD _0
+FSTP @retorno
+
+FLD _1
+FSTP @indice
 
 FLD x
 FLD _1
 FADD
 FSTP @aux2
+
+FLD @aux2
+FSTP @E
 
 FLD @E
 FLD a
@@ -68,25 +81,37 @@ FSTSW AX
 SAHF
 BNE etiqueta2
 
+FLD @indice
+FSTP @retorno
+
 etiqueta2:
 FLD @indice
-FLD @indice
+FLD _1
 FADD
 FSTP @aux3
 
+FLD @aux3
+FSTP @indice
+
 FLD @E
-FLD e
+FLD b
 FXCH
 FCOM
 FSTSW AX
 SAHF
 BNE etiqueta3
 
+FLD @indice
+FSTP @retorno
+
 etiqueta3:
 FLD @indice
-FLD @indice
+FLD _1
 FADD
 FSTP @aux4
+
+FLD @aux4
+FSTP @indice
 
 FLD @E
 FLD _3
@@ -96,11 +121,17 @@ FSTSW AX
 SAHF
 BNE etiqueta4
 
+FLD @indice
+FSTP @retorno
+
 etiqueta4:
 FLD @indice
-FLD @indice
+FLD _1
 FADD
 FSTP @aux5
+
+FLD @aux5
+FSTP @indice
 
 FLD @E
 FLD d
@@ -110,11 +141,17 @@ FSTSW AX
 SAHF
 BNE etiqueta5
 
+FLD @indice
+FSTP @retorno
+
 etiqueta5:
 FLD @indice
-FLD @indice
+FLD _1
 FADD
 FSTP @aux6
+
+FLD @aux6
+FSTP @indice
 
 FLD @retorno
 FSTP x
